00e8b9ad3f4d05ef76a372fa020d460f111597cb
Andrey Berrocal Cuadra <andrey7cr@gmail.com>
2025-04-19 16:49:08 -0600

    Se añaden nuevas estadísiticas.

--- MotoGPCampeonato/Controllers/EstadisticasController.cs
+++ MotoGPCampeonato/Controllers/EstadisticasController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MotoGPCampeonato.Data;
 using MotoGPCampeonato.Models;
@@ -45,5 +46,53 @@ public class EstadisticasController : Controller
     {
         return puntosDisputados == 0 ? 0 : (double)puntosObtenidos / puntosDisputados * 100;
     }
+
+    [HttpGet]
+    public async Task<IActionResult> PilotoVsPiloto()
+    {
+        var pilotos = await _context.Pilotos.OrderBy(p => p.Nombre).ToListAsync();
+        ViewBag.Pilotos = new SelectList(pilotos, "PilotoId", "Nombre");
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PilotoVsPiloto(int pilotoAId, int pilotoBId)
+    {
+        var pilotos = await _context.Pilotos.OrderBy(p => p.Nombre).ToListAsync();
+        ViewBag.Pilotos = new SelectList(pilotos, "PilotoId", "Nombre");
+
+        var pilotoA = await _context.Pilotos.FirstOrDefaultAsync(p => p.PilotoId == pilotoAId);
+        var pilotoB = await _context.Pilotos.FirstOrDefaultAsync(p => p.PilotoId == pilotoBId);
+
+        ViewBag.PilotoANombre = pilotoA?.Nombre ?? "Piloto A";
+        ViewBag.PilotoAPuntos = pilotoA?.Puntos ?? 0;
+
+        ViewBag.PilotoBNombre = pilotoB?.Nombre ?? "Piloto B";
+        ViewBag.PilotoBPuntos = pilotoB?.Puntos ?? 0;
+
+        return View();
+    }
+
+    public async Task<IActionResult> PodiosPorPiloto()
+    {
+        var podios = await _context.ResultadosCarrera
+            .Where(r => r.Posicion <= 3)
+            .Include(r => r.Piloto)
+            .GroupBy(r => r.Piloto.Nombre)
+            .Select(g => new
+            {
+                Nombre = g.Key,
+                Cantidad = g.Count()
+            })
+            .OrderByDescending(g => g.Cantidad)
+            .ToListAsync();
+
+        ViewBag.Nombres = string.Join(",", podios.Select(p => $"'{p.Nombre}'"));
+        ViewBag.Cantidades = string.Join(",", podios.Select(p => p.Cantidad));
+
+        return View();
+    }
+
+
 
 }
